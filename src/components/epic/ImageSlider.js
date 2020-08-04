d91144d52f219ebe1360542d185edfbae00a5b1b
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2020-08-04 18:28:38 -0500

    metadata statistics calculations

--- src/components/epic/ImageSlider.js
+++ src/components/epic/ImageSlider.js
@@ -1,9 +1,13 @@
 import { useState, useRef, useEffect } from 'react';
+import { FiChevronLeft, FiChevronRight, FiPlay, FiPause } from 'react-icons/fi';
 
-const ImageSlider = ({ current, type }) => {
-  const [currentIndex, setCurrentIndex] = useState(0);
+const ImageSlider = ({ current, type, currentIndex, setCurrentIndex }) => {
+  // const [currentIndex, setCurrentIndex] = useState(0);
   const [imageHeight, setImageHeight] = useState(1080);
+  const [playing, setPlaying] = useState(false);
   const imageRef = useRef();
+  const timerIdRef = useRef(null);
+  const indexRef = useRef(0);
   const images = current.map(item => {
     const year = item?.date.slice(0, 4);
     const month = item?.date.slice(5, 7);
@@ -25,36 +29,89 @@ const ImageSlider = ({ current, type }) => {
     return () => window.removeEventListener('resize', updateImageHeight);
   });
 
+  useEffect(() => {
+    setCurrentIndex(0);
+  }, [current]);
+
+  useEffect(() => {
+    if (playing) {
+      indexRef.current = currentIndex;
+      timerIdRef.current = setInterval(() => {
+        if (indexRef.current < images.length - 1) {
+          indexRef.current = indexRef.current + 1;
+          setCurrentIndex(indexRef.current);
+        } else {
+          indexRef.current = 0;
+          setCurrentIndex(indexRef.current);
+        }
+      }, 500);
+    } else {
+      clearInterval(timerIdRef.current);
+    }
+  }, [playing]);
+
   const handleIncrement = () => {
     if (currentIndex < images.length - 1) {
       setCurrentIndex(currentIndex + 1);
+    } else {
+      setCurrentIndex(0);
     }
   };
 
   const handleDecrement = () => {
     if (currentIndex > 0) {
       setCurrentIndex(currentIndex - 1);
+    } else {
+      setCurrentIndex(images.length - 1);
     }
+  };
+
+  const handlePlay = () => {
+    setPlaying(true);
+  };
+  const handlePause = () => {
+    setPlaying(false);
   };
 
   return (
     <>
-      <button
-        className='px-4 py-2 bg-primary rounded-lg m-2'
-        onClick={handleDecrement}
-      >
-        decrease index: {currentIndex}
-      </button>
-      <button
-        className='px-4 py-2 bg-primary rounded-lg m-2'
-        onClick={handleIncrement}
-      >
-        increase index: {currentIndex}
-      </button>
       <div
-        className='relative top-0 left-0'
-        style={{ height: `${imageHeight}px` }}
+        className='relative top-0 left-0 mx-auto'
+        style={{
+          height: `${imageHeight}px`,
+          maxWidth: `${imageHeight}px`,
+        }}
       >
+        <div className='absolute top-0 z-10 w-full text-center text-xl text-gray-400 mt-2'>
+          {currentIndex + 1}/{images.length}
+        </div>
+        <FiChevronLeft
+          className='rounded-full cursor-pointer absolute left-0 z-10 text-4xl'
+          style={{ top: '50%' }}
+          onClick={handleDecrement}
+          title='Previous Image'
+        />
+        <FiChevronRight
+          className='rounded-full cursor-pointer absolute right-0 z-10 text-4xl'
+          style={{ top: '50%' }}
+          onClick={handleIncrement}
+          title='Next Image'
+        />
+        <div className='absolute bottom-0 z-10 w-full flex justify-center'>
+          {playing ? (
+            <FiPause
+              className='rounded-full cursor-pointer text-2xl mb-2'
+              onClick={handlePause}
+              title='Play/Pause Slideshow'
+            />
+          ) : (
+            <FiPlay
+              className='rounded-full cursor-pointer text-2xl mb-2'
+              onClick={handlePlay}
+              title='Play/Pause Slideshow'
+            />
+          )}
+        </div>
         {images.map((item, index) => {
           return (
             <img
@@ -64,9 +121,10 @@ const ImageSlider = ({ current, type }) => {
               ref={imageRef}
               width='1080px'
               height='1080px'
-              className='absolute left-0 top-0'
+              className='absolute left-0 top-0 rounded-lg'
               style={{
                 zIndex: `${currentIndex === index ? 1 : 0}`,
+                maxHeight: '80vh',
               }}
             />
           );
--- src/pages/epic.js
+++ src/pages/epic.js
@@ -1,8 +1,28 @@
 import { useState, useContext, useEffect } from 'react';
 import Calendar from 'react-calendar';
+import { FiCalendar } from 'react-icons/fi';
 import EpicContext from '../context/epic/epicContext';
 import LinearProgress from '../components/LinearProgress';
 import ImageSlider from '../components/epic/ImageSlider';
+
+// distance = sqrt(x^2 + y^2 + z^2)
+const getDistance = (x, y, z) => {
+  return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
+};
+
+// Angle between 3d vectors
+const getSev = (sun, dscovr) => {
+  const dotProduct = sun.x * dscovr.x + sun.y * dscovr.y + sun.z * dscovr.z;
+  const magnitudeA = Math.sqrt(
+    Math.pow(sun.x, 2) + Math.pow(sun.y, 2) + Math.pow(sun.z, 2)
+  );
+  const magnitudeB = Math.sqrt(
+    Math.pow(dscovr.x, 2) + Math.pow(dscovr.y, 2) + Math.pow(dscovr.z, 2)
+  );
+  const cosAngle = dotProduct / (magnitudeA * magnitudeB);
+  const angle = Math.acos(cosAngle) * (180 / Math.PI);
+  return angle;
+};
 
 const epic = () => {
   const epicContext = useContext(EpicContext);
@@ -20,6 +40,26 @@ const epic = () => {
     loading,
     date,
   } = epicContext;
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const dscovr = {
+    x: currentMetadata[currentIndex]?.dscovr_j2000_position.x,
+    y: currentMetadata[currentIndex]?.dscovr_j2000_position.y,
+    z: currentMetadata[currentIndex]?.dscovr_j2000_position.z,
+  };
+  const dscovrDistance = getDistance(dscovr.x, dscovr.y, dscovr.z);
+  const sun = {
+    x: currentMetadata[currentIndex]?.sun_j2000_position.x,
+    y: currentMetadata[currentIndex]?.sun_j2000_position.y,
+    z: currentMetadata[currentIndex]?.sun_j2000_position.z,
+  };
+  const sunDistance = getDistance(sun.x, sun.y, sun.z);
+  const sevAngle = getSev(sun, dscovr);
+  // Law of cosines
+  const distanceToSun = Math.sqrt(
+    Math.pow(sunDistance, 2) +
+      Math.pow(dscovrDistance, 2) -
+      2 * sunDistance * dscovrDistance * Math.cos(sevAngle * (Math.PI / 180))
+  );
 
   const handleDateChange = value => {
     if (type === 'natural') {
@@ -33,23 +73,80 @@ const epic = () => {
     <>
       <div className='container mx-auto'>
         {loading && <LinearProgress />}
-        <p>{`error status: ${error}`}</p>
-        <p>error message: {errorMessage}</p>
-        <p>date: {date.toISOString().slice(0, 10)}</p>
-        <div>
+        <div className='flex justify-center items-center mb-4 lg:hidden'>
           <button
-            className='px-4 py-2 m-2 bg-primary'
-            onClick={getMostRecentEnhanced}
-          >
-            Enhanced
-          </button>
-          <button
-            className='px-4 py-2 m-2 bg-primary'
+            className={`text-lg px-4 py-2 border border-primary rounded-l-lg hover:bg-primary hover:text-black ${
+              type === 'natural'
+                ? 'bg-primary text-black font-semibold'
+                : 'text-primary-light'
+            }`}
             onClick={getMostRecentNatural}
           >
             Natural
           </button>
+          <button
+            className={`text-lg px-4 py-2 border border-primary border-l-black rounded-r-lg hover:bg-primary hover:text-black ${
+              type === 'enhanced'
+                ? 'bg-primary text-black font-semibold'
+                : 'text-primary-light'
+            }`}
+            onClick={getMostRecentEnhanced}
+          >
+            Enhanced
+          </button>
         </div>
+        <div className='grid gap-4 lg:grid-cols-3'>
+          <div className='block lg:col-span-2'>
+            <ImageSlider
+              current={currentMetadata}
+              type={type}
+              currentIndex={currentIndex}
+              setCurrentIndex={setCurrentIndex}
+            />
+          </div>
+          <div className='flex flex-col lg:row-start-1'>
+            <div className='justify-center items-center mb-4 hidden lg:flex'>
+              <button
+                className={`text-lg px-4 py-2 border border-primary rounded-l-lg hover:bg-primary hover:text-black ${
+                  type === 'natural'
+                    ? 'bg-primary text-black font-semibold'
+                    : 'text-primary-light'
+                }`}
+                onClick={getMostRecentNatural}
+              >
+                Natural
+              </button>
+              <button
+                className={`text-lg px-4 py-2 border border-primary border-l-black rounded-r-lg hover:bg-primary hover:text-black ${
+                  type === 'enhanced'
+                    ? 'bg-primary text-black font-semibold'
+                    : 'text-primary-light'
+                }`}
+                onClick={getMostRecentEnhanced}
+              >
+                Enhanced
+              </button>
+            </div>
+            <div className='bg-black border border-gray-700 rounded-lg p-4 lg:h-40'>
+              <div className='flex items-center text-primary cursor-pointer hover:underline'>
+                <FiCalendar className='mr-2' />
+                <p className='flex-shrink'>{date.toISOString().slice(0, 10)}</p>
+              </div>
+              <p>
+                Distance to Earth: {Math.round(dscovrDistance).toLocaleString()}{' '}
+                km
+              </p>
+              <p>
+                Distance to Sun: {Math.round(distanceToSun).toLocaleString()} km
+              </p>
+              <p>Sun to Earth: {Math.round(sunDistance).toLocaleString()} km</p>
+              <p>SEV Angle: {sevAngle.toFixed(2)}&deg;</p>
+            </div>
+          </div>
+        </div>
+        <p>{`error status: ${error}`}</p>
+        <p>error message: {errorMessage}</p>
+        <p>date: {date.toISOString().slice(0, 10)}</p>
         <div className=''>
           <Calendar
             onChange={(value, event) => handleDateChange(value, event)}
@@ -60,7 +157,6 @@ const epic = () => {
             tileClassName=''
           />
         </div>
-        <ImageSlider current={currentMetadata} type={type} />
         <div className='flex flex-wrap'>
           {currentMetadata?.map(item => {
             const year = item?.date.slice(0, 4);
@@ -83,17 +179,7 @@ const epic = () => {
           })}
         </div>
       </div>
-      <style jsx>{`
-        input[type='date']::-webkit-calendar-picker-indicator {
-          background-color: white;
-          color: white;
-          opacity: 1;
-        }
-        input[type='date']::-webkit-calendar-picker-indicator::after {
-          color: white;
-          opacity: 1;
-        }
-      `}</style>
+      <style jsx>{``}</style>
     </>
   );
 };
