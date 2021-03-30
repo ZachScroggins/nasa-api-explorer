ea5edf9e691fd52fb8c99d464a41e895e914ec17
ZachScroggins <58402189+ZachScroggins@users.noreply.github.com>
2021-03-30 16:42:15 -0500

    typescript ui components

--- components/ui/Rocket/Rocket.tsx
+++ components/ui/Rocket/Rocket.tsx
@@ -1,4 +1,4 @@
-const Rocket = props => {
+const Rocket: React.FC<React.SVGProps<SVGSVGElement>> = props => {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
--- components/ui/RocketIcon/RocketIcon.tsx
+++ components/ui/RocketIcon/RocketIcon.tsx
@@ -1,4 +1,4 @@
-const RocketIcon = props => {
+const RocketIcon: React.FC<React.SVGProps<SVGSVGElement>> = props => {
   return (
     <svg viewBox='0 0 512 512' {...props}>
       <defs>
